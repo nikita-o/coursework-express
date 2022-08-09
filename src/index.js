917080c72c2cdf58af6b0cdf1fe700cef32fe75f
Nik <nik.oschepkov@yandex.ru>
2022-08-09 14:44:06 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import { HTTP_HOST, HTTP_PORT, MONGO_URL } from './config.js';
+import { createServer } from 'http';
+import mongoose from 'mongoose';
+
+const app = express()
+
+async function start() {
+    try {
+        const server = createServer(app);
+        await mongoose.connect(MONGO_URL);
+        server.listen(HTTP_PORT, () => {
+            console.log(`Server: http://${HTTP_HOST}:${HTTP_PORT}`);
+        })
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+start();
